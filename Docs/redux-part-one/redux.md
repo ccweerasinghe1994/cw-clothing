1bd5ebb4527d8745ab24a1f027ef5d6b44c30a7c
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-16 23:02:01 +0530

    count value added to the  shopping icon

--- Docs/redux-part-one/redux.md
+++ Docs/redux-part-one/redux.md
@@ -1087,3 +1087,31 @@ const mapStateToProps = ({ cart: { cardItems } }) => ({ cardItems });
 export default connect(mapStateToProps)(CartDropDown);
 
 ```
+
+adding count value to the cartIcon 
+```jsx
+import React from "react";
+import "./cart-icon.style.scss";
+import { ReactComponent as ShoppingIcon } from "../../assets/shoopingBag.svg";
+import { connect } from "react-redux";
+import { toggleCardHidden } from "../../redux/cart/cart.action";
+const CartIcon = ({ toggleCardHidden, count }) => {
+  return (
+    <div className="cart-icon" onClick={toggleCardHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{count}</span>
+    </div>
+  );
+};
+const mapDispatchToProps = dispatch => ({
+  toggleCardHidden: () => dispatch(toggleCardHidden())
+});
+const mapStateToProps = ({ cart: { cardItems } }) => ({
+  count: cardItems.reduce(
+    (accumilatedValue, currentValue) =>
+      accumilatedValue + currentValue.quantity,
+    0
+  )
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+```
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import "./cart-icon.style.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shoopingBag.svg";
 import { connect } from "react-redux";
-import {toggleCardHidden} from '../../redux/cart/cart.action'
-const CartIcon = ({toggleCardHidden}) => {
+import { toggleCardHidden } from "../../redux/cart/cart.action";
+const CartIcon = ({ toggleCardHidden, count }) => {
   return (
     <div className="cart-icon" onClick={toggleCardHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{count}</span>
     </div>
   );
 };
 const mapDispatchToProps = dispatch => ({
   toggleCardHidden: () => dispatch(toggleCardHidden())
 });
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cardItems } }) => ({
+  count: cardItems.reduce(
+    (accumilatedValue, currentValue) =>
+      accumilatedValue + currentValue.quantity,
+    0
+  )
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
