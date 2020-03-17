e44d6e055051d7ef21024a26209330e5e18baf3a
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-18 01:41:55 +0530

    adding new nested routes for the collection page

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,22 @@
 import { createSelector } from "reselect";
 
+const COLLECTION_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5
+};
+
 const selctShop = state => state.shop;
 
 export const selelctCollections = createSelector(
   [selctShop],
   shop => shop.collections
 );
+export const selectCollection = collectionUrlParam =>
+  createSelector([selelctCollections], collections =>
+    collections.find(
+      collection => collection.id === COLLECTION_MAP[collectionUrlParam]
+    )
+  );
