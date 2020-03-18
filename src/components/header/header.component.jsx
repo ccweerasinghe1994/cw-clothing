a6dc0a8c25ad6df7d72a3b5ade06d4b92e523c96
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-03-18 18:48:17 +0530

    removing unnessery scs

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./header.style.scss";
-import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
